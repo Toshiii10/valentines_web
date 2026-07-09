35b37eaffba1fffbe59464f593c4d5f27c49e755
Carlito <carljosephjmanantan@iskolarngbayan.pup.edu.ph>
2026-07-09 19:40:16 +0800

    Add service worker for asset caching
    Implement service worker for caching and offline support.

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "valentine-app-v1";
+const assetsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./data.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./YOUR_AUDIO_FILE.mp3" // Change this to your actual song file name!
+];
+
+// Install the service worker and cache the files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Caching app assets");
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch files from the cache if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Return the cached file if found, otherwise fetch from the network
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("Clearing old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
