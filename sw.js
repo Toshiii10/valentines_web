552f77063dc77367604e7e2d7cfab5d47b63afa1
Carlito <carljosephjmanantan@iskolarngbayan.pup.edu.ph>
2026-07-21 20:03:36 +0800

    Refactor service worker to handle fetch events
    Removed old cache cleanup logic and added fetch event listener for caching responses.

--- sw.js
+++ sw.js
@@ -31,17 +31,16 @@ self.addEventListener("fetch", event => {
 });
 
 // Activate and clean up old caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log("Clearing old cache");
-            return caches.delete(cache);
-          }
-        })
-      );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
     })
   );
 });
